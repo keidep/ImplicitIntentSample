d030071f6a81239e8f350b224892eca4a121c9d5
keidep <key103027@gmail.com>
2025-05-07 15:28:58 +0900

    Map_add

--- app/src/main/java/com/example/implicitintentsample/MainActivity.java
+++ app/src/main/java/com/example/implicitintentsample/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.implicitintentsample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +23,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void onMapSearchButtonClick(View view){
+        //入力欄に入力されたキーワード文字列を取得。
+        EditText etSearchWord = findViewById(R.id.etSearchWord);
+        String searchWord = etSearchWord.getText().toString();
+
+        try {
+            //入力されたキーワードをURLエンコード。
+            searchWord = URLEncoder.encode(searchWord, "UTF-8");
+            //マップアプリと連携するURI文字列を生成。
+            String uriStr = "geo:0,0?q=" + searchWord;
+            //URI文字列からURIオブジェクトを生成。
+            Uri uri = Uri.parse(uriStr);
+            //Intentオブジェクトを生成。
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            //アクティビティを起動。
+            startActivity(intent);
+        }
+        catch (UnsupportedEncodingException ex) {
+            Log.e("MainActivity","検索キーワード変換失敗", ex);
+        }
     }
 }
